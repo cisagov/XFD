d9566b6f2c7394c42a77fc14d22e255f9cbe354e
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-04-08 15:46:26 -0400

    Added responsive design to RSCHeader.tsx

--- frontend/src/components/ReadySetCyber/RSCHeader.tsx
+++ frontend/src/components/ReadySetCyber/RSCHeader.tsx
@@ -12,7 +12,8 @@ import {
   MenuItem,
   Tooltip
 } from '@mui/material';
-import { AccountCircle } from '@mui/icons-material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
 import RSCLogo from 'components/ReadySetCyber/Assets/ReadySetCyberLogo.png';
 
 export const RSCHeader: React.FC = () => {
@@ -23,9 +24,20 @@ export const RSCHeader: React.FC = () => {
   const { logout, user } = useAuthContext();
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
   };
 
   const handleClose = () => {
@@ -36,9 +48,12 @@ export const RSCHeader: React.FC = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ bgcolor: 'white' }}>
         <Container maxWidth="xl">
-          <Toolbar>
+          <Toolbar disableGutters>
             <Tooltip title="Go to Ready Set Cyber" arrow>
-              <IconButton onClick={handleClick}>
+              <IconButton
+                onClick={handleClick}
+                sx={{ display: { xs: 'none', md: 'flex' } }}
+              >
                 <img
                   src={RSCLogo}
                   alt="Ready Set Cyber Logo"
@@ -53,10 +68,59 @@ export const RSCHeader: React.FC = () => {
             <Typography
               variant="h6"
               component="div"
-              sx={{ flexGrow: 1, color: '#07648D' }}
+              sx={{
+                flexGrow: 1,
+                color: '#07648D',
+                display: { xs: 'none', md: 'flex' }
+              }}
             >
               Dashboard
             </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="primary"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+              >
+                <MenuItem>Dashboard</MenuItem>
+              </Menu>
+            </Box>
+            <Tooltip title="Go to Ready Set Cyber" arrow>
+              <IconButton
+                onClick={handleClick}
+                sx={{ display: { xs: 'flex', md: 'none' } }}
+              >
+                <img
+                  src={RSCLogo}
+                  alt="Ready Set Cyber Logo"
+                  style={{
+                    width: '100px',
+                    height: '50px',
+                    marginRight: '10px'
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
             {user && (
               <div>
                 <Tooltip title="Open Menu" arrow>
