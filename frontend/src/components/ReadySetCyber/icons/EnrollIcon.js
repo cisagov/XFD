2df275f18893838cac14aaea5f2184bb57ea84e3
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-04-25 16:27:28 -0400

    Fixed linintng errors in ContactIcon & EnrollIcon

--- frontend/src/components/ReadySetCyber/icons/EnrollIcon.js
+++ frontend/src/components/ReadySetCyber/icons/EnrollIcon.js
@@ -14,23 +14,23 @@ export default function EnrollIcon({ size = 50 }) {
           id="Vector"
           d="M14.1582 25.5C16.9262 30.278 22.0812 33.5 28.0002 33.5C36.8372 33.5 44.0002 26.337 44.0002 17.5C44.0002 8.663 36.8372 1.5 28.0002 1.5C22.0812 1.5 16.9262 4.722 14.1582 9.5"
           stroke="#0078AE"
-          stroke-width="3"
-          stroke-miterlimit="10"
+          strokeWidth="3"
+          strokeMiterlimit="10"
         />
         <path
           id="Vector_2"
           d="M0 17.5H34"
           stroke="#C41230"
-          stroke-width="3"
-          stroke-miterlimit="10"
+          strokeWidth="3"
+          strokeMiterlimit="10"
         />
         <path
           id="Vector_3"
           d="M27 10.5L34 17.5L27 24.5"
           stroke="#C41230"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="square"
+          strokeWidth="3"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
         />
       </g>
     </svg>
