62c414d5ec802ccc5da79b5f2c4c0fd20ecf7a4c
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2024-05-07 16:25:58 -0500

    Fix frontend lint issues.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -368,10 +368,11 @@ const HeaderNoCtx: React.FC<ContextType> = (props) => {
                       ) {
                         return options;
                       }
-                      return options.filter((option) =>
-                        option?.name
-                          .toLowerCase()
-                          .includes(state.inputValue.toLowerCase())
+                      return options.filter(
+                        (option) =>
+                          option?.name
+                            .toLowerCase()
+                            .includes(state.inputValue.toLowerCase())
                       );
                     }}
                     disableClearable
