cfb4c67a77e09b38c12ecd0d783b7de3b021fdca
JCantu248 <jcantu248@gmail.com>
2024-05-14 14:43:29 -0500

    Fix linter errors in backend/src/models/notifications.ts

--- backend/src/models/notification.ts
+++ backend/src/models/notification.ts
@@ -1,57 +1,56 @@
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    BaseEntity,
-    CreateDateColumn,
-    UpdateDateColumn
-  } from 'typeorm';
-  
-  @Entity()
-  export class Notification extends BaseEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-  
-    @CreateDateColumn()
-    createdAt: Date;
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm';
 
-    @UpdateDateColumn()
-    updatedAt: Date;
-  
-    @Column({
-      type: 'timestamp',
-      nullable: true
-    })
-    startDatetime: Date | null;
+@Entity()
+export class Notification extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({
-        type: 'timestamp',
-        nullable: true
-      })
-      endDatetime: Date | null;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @Column({
-      nullable: true,
-      type: 'varchar'
-    })
-    maintenanceType: string | null;
-  
-    @Column({
-      nullable: true,
-      type: 'varchar'
-    })
-    status: string | null;
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-    @Column({
-        nullable: true,
-        type: 'varchar'
-      })
-    updatedBy: string | null;
+  @Column({
+    type: 'timestamp',
+    nullable: true
+  })
+  startDatetime: Date | null;
 
-    @Column({
-        nullable: true,
-        type: 'varchar'
-      })
-      message: string | null;
+  @Column({
+    type: 'timestamp',
+    nullable: true
+  })
+  endDatetime: Date | null;
 
-  }
+  @Column({
+    nullable: true,
+    type: 'varchar'
+  })
+  maintenanceType: string | null;
+
+  @Column({
+    nullable: true,
+    type: 'varchar'
+  })
+  status: string | null;
+
+  @Column({
+    nullable: true,
+    type: 'varchar'
+  })
+  updatedBy: string | null;
+
+  @Column({
+    nullable: true,
+    type: 'varchar'
+  })
+  message: string | null;
+}
