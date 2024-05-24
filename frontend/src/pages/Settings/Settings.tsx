c0b0578d90c6783c58ef098af0f6a12d47f1c9bd
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-05-24 11:31:42 -0400

    Removed Generate API/ Matamo buttons/functionality
    - Removed Generate API button and functionality.
    - Removed Matamo button and functionality.
    - Removed modal, columns.

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -1,130 +1,11 @@
 import React, { useRef, useState } from 'react';
 import classes from './Settings.module.css';
 import { useAuthContext } from 'context';
-import {
-  Button,
-  ButtonGroup,
-  Modal,
-  ModalFooter,
-  ModalHeading,
-  ModalRef
-} from '@trussworks/react-uswds';
-import { ModalToggleButton } from 'components';
-import { Table } from 'components';
-import { ApiKey } from 'types/api-key';
-import { Column } from 'react-table';
-import { FaTimes } from 'react-icons/fa';
-// @ts-ignore:next-line
-import { formatDistanceToNow, parseISO } from 'date-fns';
+import { Button, ModalRef } from '@trussworks/react-uswds';
 
 const Settings: React.FC = () => {
-  const { logout, user, setUser, apiPost, apiDelete } = useAuthContext();
-  const [apiKey, setApiKey] = useState<string>('');
+  const { logout, user } = useAuthContext();
   const modalRef = useRef<ModalRef>(null);
-
-  const generateApiKey = async () => {
-    if (!user) return;
-    const apiKey = await apiPost<
-      ApiKey & {
-        key: string;
-      }
-    >('/api-keys');
-    setUser({ ...user, apiKeys: user.apiKeys.concat([apiKey]) });
-    setApiKey(apiKey.key);
-    modalRef.current?.toggleModal(undefined, true);
-  };
-
-  const deleteApiKey = async (key: string) => {
-    if (!user) return;
-    await apiDelete<ApiKey>('/api-keys/' + key);
-    setUser({
-      ...user,
-      apiKeys: user.apiKeys.filter((k) => k.id !== key)
-    });
-  };
-
-  const columns: Column<ApiKey>[] = [
-    {
-      Header: 'Key',
-      accessor: ({ lastFour }) => '*'.repeat(12) + lastFour,
-      width: 200,
-      disableFilters: true,
-      id: 'key'
-    },
-    {
-      Header: 'Date Created',
-      accessor: ({ createdAt }) =>
-        `${formatDistanceToNow(parseISO(createdAt))} ago`,
-      width: 50,
-      minWidth: 50,
-      id: 'createdAt',
-      disableFilters: true
-    },
-    {
-      Header: 'Last Used',
-      accessor: ({ lastUsed }) =>
-        lastUsed ? `${formatDistanceToNow(parseISO(lastUsed))} ago` : 'None',
-      width: 50,
-      minWidth: 50,
-      id: 'lastUsed',
-      disableFilters: true
-    },
-    {
-      Header: 'Delete',
-      id: 'delete',
-      Cell: ({ row }: { row: { index: number } }) => (
-        <span
-          onClick={() => {
-            if (!user) return;
-            deleteApiKey(user.apiKeys[row.index].id);
-          }}
-        >
-          <FaTimes />
-        </span>
-      ),
-      disableFilters: true
-    }
-  ];
-
-  const apiKeysJsx = (
-    <>
-      <h2>API Keys:</h2>
-      <p>
-        <a
-          href="https://docs.crossfeed.cyber.dhs.gov/api-reference"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Read API documentation
-        </a>
-      </p>
-      {(!user?.apiKeys || user.apiKeys.length === 0) && <p>No API Keys</p>}
-      {user?.apiKeys && user.apiKeys.length > 0 && (
-        <Table<ApiKey> columns={columns} data={user?.apiKeys} />
-      )}
-      <br></br>
-      <Button type="button" onClick={generateApiKey}>
-        Generate API Key
-      </Button>
-      <br></br>
-      <br></br>
-
-      <Modal ref={modalRef} id="modal">
-        <ModalHeading>Copy API Key</ModalHeading>
-        <p>
-          Please copy your API key now, as you will not be able to see it again:
-        </p>
-        <code>{apiKey}</code>
-        <ModalFooter>
-          <ButtonGroup>
-            <ModalToggleButton modalRef={modalRef} closer>
-              Ok
-            </ModalToggleButton>
-          </ButtonGroup>
-        </ModalFooter>
-      </Modal>
-    </>
-  );
 
   return (
     <div className={classes.root}>
@@ -139,16 +20,6 @@ const Settings: React.FC = () => {
             .map((role) => role.organization.name)
             .join(', ')}
       </h2>
-      {user?.userType !== 'standard' && apiKeysJsx}
-      {user?.userType === 'globalAdmin' && (
-        <>
-          <a href={`${process.env.REACT_APP_API_URL}/matomo/index.php`}>
-            <Button type="button">Matomo</Button>
-          </a>
-          <br />
-          <br />
-        </>
-      )}
       <Button type="button" onClick={logout}>
         Logout
       </Button>
