27abfbff3498276568a24b45f63369a23856ee5c
nickviola <nick.viola@associates.cisa.dhs.gov>
2024-06-12 15:08:46 -0500

    Remove minio local s3 instance since service is not working.

--- backend/src/tasks/s3-client.ts
+++ backend/src/tasks/s3-client.ts
@@ -11,14 +11,16 @@ class S3Client {
   isLocal: boolean;
 
   constructor(isLocal?: boolean) {
+    // TODO: Remove once minio service is fixed
     // this.isLocal =
     //   isLocal ??
     //   (process.env.IS_OFFLINE || process.env.IS_LOCAL ? true : false);
     if (this.isLocal) {
-      this.s3 = new S3({
-        endpoint: 'http://minio:9000',
-        s3ForcePathStyle: true
-      });
+      // TODO: Remove once minio service is fixed
+      // this.s3 = new S3({
+      //   endpoint: 'http://minio:9000',
+      //   s3ForcePathStyle: true
+      // });
     } else {
       const agent = new https.Agent({
         keepAlive: false
