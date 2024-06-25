5643b304f2a0dc59b360e3b8c660b33bea32be14
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-06-25 10:25:43 -0400

    Removed old React table and cleaned up code.
    - Removed old table, old pagination, and old expanded row.
    - Removed old imports.
    - Removed old helper functions.

--- frontend/src/pages/Scans/ScanTasksView.tsx
+++ frontend/src/pages/Scans/ScanTasksView.tsx
@@ -1,13 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { TableInstance, Column, CellProps, Row } from 'react-table';
 import { Query, Scan } from 'types';
-import { Table, Paginator, ColumnFilter, selectFilter } from 'components';
 import { ScanTask } from 'types';
 import { useAuthContext } from 'context';
 // @ts-ignore:next-line
-import { formatDistanceToNow, parseISO, set } from 'date-fns';
+import { formatDistanceToNow, parseISO } from 'date-fns';
 import classes from './Scans.module.scss';
-import { FaMinus, FaPlus, FaSyncAlt } from 'react-icons/fa';
+import { FaSyncAlt } from 'react-icons/fa';
 import { LazyLog } from 'react-lazylog';
 import { Button } from '@trussworks/react-uswds';
 import {
@@ -116,159 +114,7 @@ export const ScanTasksView: React.FC = () => {
       console.log(e);
     }
   };
-  const killScanTask = async (index: number) => {
-    try {
-      const row = scanTasks[index];
-      await apiPost(`/scan-tasks/${row.id}/kill`, { body: {} });
-      setScanTasks(
-        Object.assign([], scanTasks, {
-          [index]: {
-            ...row,
-            status: 'failed'
-          }
-        })
-      );
-    } catch (e: any) {
-      setErrors({
-        global:
-          e.status === 422 ? 'Unable to kill scan' : e.message ?? e.toString()
-      });
-      console.log(e);
-    }
-  };
 
-  const renderExpanded = (row: Row<ScanTask>) => {
-    const { original } = row;
-    return (
-      <div className={classes.expandedRoot}>
-        {original.fargateTaskArn && (
-          <>
-            <h4>
-              Logs
-              {original.fargateTaskArn?.match('.*/(.*)') && (
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={`https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups/log-group/${process
-                    .env
-                    .REACT_APP_FARGATE_LOG_GROUP!}/log-events/worker$252Fmain$252F${
-                    (original.fargateTaskArn.match('.*/(.*)') || [])[1]
-                  }`}
-                >
-                  {' '}
-                  (View all on CloudWatch)
-                </a>
-              )}
-            </h4>
-
-            <Log
-              token={token ?? ''}
-              url={`${process.env.REACT_APP_API_URL}/scan-tasks/${original.id}/logs`}
-            />
-          </>
-        )}
-
-        <h4>Input</h4>
-        <small>
-          <pre>{JSON.stringify(JSON.parse(original.input), null, 2)}</pre>
-        </small>
-        <h4>Output</h4>
-        <small>
-          <pre>{original.output || 'None'}</pre>
-        </small>
-
-        {row.original.status !== 'finished' &&
-          row.original.status !== 'failed' && (
-            <>
-              <h4>Actions</h4>
-              <a
-                href="# "
-                onClick={(e) => {
-                  e.preventDefault();
-                  killScanTask(row.index);
-                }}
-              >
-                Kill
-              </a>
-            </>
-          )}
-      </div>
-    );
-  };
-
-  const columns: Column<ScanTask>[] = [
-    {
-      Header: 'ID',
-      accessor: 'id',
-      Filter: ColumnFilter,
-      disableSortBy: true,
-      disableFilters: true
-    },
-    {
-      Header: 'Status',
-      accessor: 'status',
-      Filter: selectFilter([
-        'created',
-        'queued',
-        'requested',
-        'started',
-        'finished',
-        'failed'
-      ]),
-      disableSortBy: true
-    },
-    {
-      Header: 'Name',
-      id: 'name',
-      accessor: ({ scan }) => scan?.name,
-      Filter: selectFilter([
-        // TODO: sync this with the SCAN_SCHEMA
-        'censys',
-        'amass',
-        'findomain',
-        'portscanner',
-        'wappalyzer',
-        'censysIpv4',
-        'censysCertificates',
-        'sslyze',
-        'searchSync',
-        'cve',
-        'dotgov',
-        'webscraper',
-        'intrigueIdent',
-        'shodan',
-        'hibp',
-        'lookingGlass',
-        'dnstwist',
-        'rootDomainSync'
-      ]),
-      disableSortBy: true
-    },
-    {
-      Header: 'Created At',
-      id: 'createdAt',
-      accessor: ({ createdAt }) => dateAccessor(createdAt),
-      disableFilters: true
-    },
-    {
-      Header: 'Finished At',
-      id: 'finishedAt',
-      accessor: ({ finishedAt }) => dateAccessor(finishedAt),
-      disableFilters: true
-    },
-    {
-      Header: 'Details',
-      Cell: ({ row }: CellProps<ScanTask>) => (
-        <span
-          {...row.getToggleRowExpandedProps()}
-          className="text-center display-block"
-        >
-          {row.isExpanded ? <FaMinus /> : <FaPlus />}
-        </span>
-      ),
-      disableFilters: true
-    }
-  ];
   const PAGE_SIZE = 15;
 
   const fetchScanTasks = useCallback(
@@ -318,10 +164,6 @@ export const ScanTasksView: React.FC = () => {
       }
     },
     [apiPost, currentOrganization, showAllOrganizations]
-  );
-
-  const renderPagination = (table: TableInstance<ScanTask>) => (
-    <Paginator table={table} totalResults={totalResults} />
   );
 
   //New Table for Scans
@@ -511,21 +353,6 @@ export const ScanTasksView: React.FC = () => {
   return (
     <>
       {errors.global && <p className={classes.error}>{errors.global}</p>}
-      <Table<ScanTask>
-        renderPagination={renderPagination}
-        columns={columns}
-        data={scanTasks}
-        pageCount={Math.ceil(totalResults / PAGE_SIZE)}
-        fetchData={fetchScanTasks}
-        pageSize={PAGE_SIZE}
-        initialSortBy={[
-          {
-            id: 'createdAt',
-            desc: true
-          }
-        ]}
-        renderExpanded={renderExpanded}
-      />
       <Box mb={3}>
         <Paper elevation={0}>
           {scanTasks?.length === 0 ? (
