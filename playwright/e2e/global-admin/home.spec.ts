d6be7a1e04f398a6fd8cd7019957a0806e233271
JCantu248 <jcantu248@gmail.com>
2024-06-25 22:17:10 -0500

    Refactor Playwright login script for Login.gov, add axe-core accessibility tests, refactor tests.

--- playwright/e2e/global-admin/home.spec.ts
+++ playwright/e2e/global-admin/home.spec.ts
@@ -1,66 +1,33 @@
-import { test, expect, Page } from '@playwright/test';
-import exp from 'constants';
+import { describe } from 'node:test';
 
-test.describe.configure({ mode: 'serial' });
-let page: Page;
+const { test, expect, Page } = require('../../axe-test');
 
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-  await page.goto('/');
-});
+test.describe.configure({ mode: 'parallel' });
+let page: InstanceType<typeof Page>;
 
-test.afterAll(async () => {
-  await page.close();
-});
-test('home', async () => {
-  // Expect home page to show Latest Vulnerabilities.
-  await expect(
-    page.getByRole('heading', { name: 'Latest Vulnerabilities' })
-  ).toBeVisible();
-  await expect(
-    page.getByRole('heading', { name: 'Open Vulnerabilities by Domain' })
-  ).toBeVisible();
-  await expect(
-    page.getByRole('heading', { name: 'Most Common Ports' })
-  ).toBeVisible();
-  await expect(
-    page.getByRole('heading', { name: 'Most Common Vulnerabilities' })
-  ).toBeVisible();
-  await expect(
-    page.getByRole('heading', { name: 'Severity Levels' })
-  ).toBeVisible();
-  await expect(
-    page.getByPlaceholder('Search a domain, vuln, port, service, IP')
-  ).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Inventory' })).toBeVisible();
-  await page.screenshot({ path: 'test-results/img/global-admin/home.png' });
-});
-
-test('Open Vulnerabilities by Domain', async () => {
-  await page.getByRole('button', { name: 'All' }).click();
-  await page.screenshot({
-    path: 'test-results/img/global-admin/open_vuln_all.png'
+test.describe('home', () => {
+  test.beforeEach(async ({ browser }) => {
+    const context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto('/');
   });
-  if (
-    (await page.getByRole('button', { name: 'Medium' }).isDisabled()) == false
-  ) {
-    await page.getByRole('button', { name: 'Medium' }).click();
-    await page.screenshot({
-      path: 'test-results/img/global-admin/open_vuln_medium.png'
+
+  test.afterEach(async () => {
+    await page.close();
+  });
+
+  test('Test homepage accessibility', async ({
+    page,
+    makeAxeBuilder
+  }, testInfo) => {
+    const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json'
     });
-  }
-  if (
-    (await page.getByRole('button', { name: 'High' }).isDisabled()) == false
-  ) {
-    await page.getByRole('button', { name: 'High' }).click();
-    await page.screenshot({
-      path: 'test-results/img/global-admin/open_vuln_high.png'
-    });
-  }
-  if ((await page.getByLabel('Go to next page').isDisabled()) == false) {
-    await page.getByLabel('Go to next page').click();
-  }
-  if ((await page.getByLabel('Go to previous page').isDisabled()) == false) {
-    await page.getByLabel('Go to previous page').click();
-  }
+
+    expect.soft(accessibilityScanResults.violations).toHaveLength(0);
+    await page.screenshot({ path: 'test-results/img/global-admin/home.png' });
+  });
 });
--- playwright/e2e/global-admin/vulnerabilities.spec.ts
+++ playwright/e2e/global-admin/vulnerabilities.spec.ts
@@ -1,59 +1,72 @@
-import { test, expect, chromium, Page } from '@playwright/test';
+const { test, expect, Page } = require('../../axe-test');
 
-test.describe.configure({ mode: 'serial' });
-let page: Page;
+test.describe.configure({ mode: 'parallel' });
+let page: InstanceType<typeof Page>;
 
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-  await page.goto('/');
-});
-
-test.afterAll(async () => {
-  await page.close();
-});
-
-test('Vulnerabilities', async () => {
-  await page.getByRole('link', { name: 'Inventory' }).click();
-  await page.getByRole('link', { name: 'All Vulnerabilities' }).click();
-  await expect(page).toHaveURL('/inventory/vulnerabilities');
-  if ((await page.getByLabel('Go to next page').isDisabled()) == false) {
-    await page.getByLabel('Go to next page').click();
-  }
-  if ((await page.getByLabel('Go to previous page').isDisabled()) == false) {
-    await page.getByLabel('Go to previous page').click();
-  }
-  await page.screenshot({
-    path: 'test-results/img/global-admin/vulnerabilities.png'
+test.describe('Vulnerabilities', () => {
+  test.beforeEach(async ({ browser }) => {
+    const context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto('/');
   });
-});
 
-test('Vulnerability details NIST', async () => {
-  await page.goto('/inventory/vulnerabilities');
-  const newTabPromise = page.waitForEvent('popup');
-  await page.getByRole('row').nth(2).getByRole('link').nth(0).click();
-  const newTab = await newTabPromise;
-  await newTab.waitForLoadState();
-  await expect(newTab).toHaveURL(
-    new RegExp('^https://nvd\\.nist\\.gov/vuln/detail/')
-  );
-});
+  test.afterEach(async () => {
+    await page.close();
+  });
 
-test('Domain details link', async () => {
-  await page.goto('/inventory/vulnerabilities');
-  await page.getByRole('row').nth(2).getByRole('link').nth(1).click();
-  await expect(page).toHaveURL(new RegExp('/inventory/domain/'));
-});
+  test('Test vulnerabilities accessibility', async ({
+    makeAxeBuilder
+  }, testInfo) => {
+    await page.getByRole('link', { name: 'Inventory' }).click();
+    await page.getByRole('link', { name: 'All Vulnerabilities' }).click();
+    await expect(page).toHaveURL('/inventory/vulnerabilities');
 
-test('Vulnerability details', async () => {
-  await page.goto('/inventory/vulnerabilities');
-  await page.getByRole('row').nth(2).getByRole('link').nth(2).click();
-  await expect(page).toHaveURL(new RegExp('/inventory/vulnerability/'));
-  await expect(page.getByRole('heading', { name: 'Overview' })).toBeVisible();
-  await expect(
-    page.getByRole('heading', { name: 'Installed (Known) Products' })
-  ).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Provenance' })).toBeVisible();
-  await expect(
-    page.getByRole('heading', { name: 'Vulnerability Detection History' })
-  ).toBeVisible();
+    const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json'
+    });
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+
+    await page.screenshot({
+      path: 'test-results/img/global-admin/vulnerabilities.png'
+    });
+  });
+
+  test('Test vulnerability details NIST link', async () => {
+    await page.goto('/inventory/vulnerabilities');
+    const newTabPromise = page.waitForEvent('popup');
+
+    await page.getByRole('row').nth(1).getByRole('cell').nth(0).click();
+    const newTab = await newTabPromise;
+    await newTab.waitForLoadState();
+    await expect(newTab).toHaveURL(
+      new RegExp('^https://nvd\\.nist\\.gov/vuln/detail/')
+    );
+  });
+
+  test('Test domain details link', async () => {
+    await page.goto('/inventory/vulnerabilities');
+    await page.getByRole('row').nth(1).getByRole('cell').nth(3).click();
+    await expect(page).toHaveURL(new RegExp('/inventory/domain/'));
+  });
+
+  test('Test vulnerability details accessibility', async ({
+    makeAxeBuilder
+  }, testInfo) => {
+    await page.goto('/inventory/vulnerabilities');
+    await page.getByRole('row').nth(1).getByRole('cell').nth(7).click();
+    await expect(page).toHaveURL(new RegExp('/inventory/vulnerability/'));
+
+    const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json'
+    });
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 });
