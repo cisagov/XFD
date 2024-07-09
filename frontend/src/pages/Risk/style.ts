f6667ae9ebe07444fe9b8d2ab0230a6afd33b67d
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-07-09 14:57:22 -0400

    Increased contrast for "See all" links in Overview
    - Increased contrast for "See all" links in Overview page.
    - Changed font color of "p" tag to "4e4e4e".
    - Wrapped Link component in Vulnerability Card in a "p" tag for consistency.

--- frontend/src/pages/Risk/style.ts
+++ frontend/src/pages/Risk/style.ts
@@ -123,7 +123,7 @@ export const RiskRoot = styled('div')(({ theme }) => ({
     marginTop: '5px',
     marginRight: '20px',
     '& p a': {
-      color: '#71767A',
+      color: '#4e4e4e',
       fontSize: '12px',
       fontWeight: '400'
     }
