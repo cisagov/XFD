55b44ec053c15ae45216101291724eebca098fbd
Janson Bunce <janson.bunce@associates.cisa.dhs.gov>
2024-07-10 13:26:08 -0700

    Adds variant and responsive font sizing

--- frontend/src/context/Theme.tsx
+++ frontend/src/context/Theme.tsx
@@ -3,13 +3,33 @@ import {
   createTheme,
   ThemeProvider,
   Theme,
-  StyledEngineProvider
+  StyledEngineProvider,
+  responsiveFontSizes, 
+  
 } from '@mui/material/styles';
+import { TypographyOptions } from '@mui/material/styles/createTypography';
+import { fontSize, ThemeOptions } from '@mui/system';
 
 declare module '@mui/material/styles' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
+  interface DefaultTheme extends Theme {
+  }
+  interface TypographyVariants { 
+    testVariant: React.CSSProperties
+  }
 }
+
+interface ExtendedTypographyOptions extends TypographyOptions {
+  testVariant: React.CSSProperties
+}
+
+declare module '@mui/material/typography' {
+  interface TypographyPropsVariantOverrides {
+    // Required to allow override of the rendered tag
+    testVariant: true
+  }
+}
+
 
 const theme = createTheme({
   palette: {
@@ -31,8 +51,31 @@ const theme = createTheme({
       lg: 1330,
       xl: 1920
     }
-  }
+  },
+  typography:{
+    testVariant: {
+      fontSize: '1.2rem'
+    }
+  } as ExtendedTypographyOptions,
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          testVariant: 'h1'
+        }
+      }
+     }
+  } 
 });
+
+theme.typography.testVariant = {
+  fontSize: '1.2rem',
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2.4rem',
+  },
+  
+}
+
 
 interface CFThemeProviderProps {
   children: React.ReactNode;
