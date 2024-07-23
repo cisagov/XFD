c1d3d02df60c1f62d02851f148fce98e515133aa
Louis Wersiy <wersiyl@mac-0006026.mynetworksettings.com>
2024-07-23 13:59:39 -0400

    updated the lastloggedin with parseISO to indicate accurate dat and time a user last logged in

--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -27,7 +27,7 @@ import { ImportExport } from 'components';
 import { initializeUser, Organization, User } from 'types';
 import { useAuthContext } from 'context';
 import { STATE_OPTIONS } from '../../constants/constants';
-import { formatDistanceToNow, parseISO } from 'date-fns';
+import { parseISO, format } from 'date-fns';
 
 type ErrorStates = {
   getUsersError: string;
@@ -104,10 +104,10 @@ export const Users: React.FC = () => {
       const rows = await apiGet<UserType[]>(`/users/`);
       rows.forEach((row) => {
         row.lastLoggedInString = row.lastLoggedIn
-          ? `${formatDistanceToNow(parseISO(row.lastLoggedIn))} ago`
+          ? format(parseISO(row.lastLoggedIn), "MM-dd-yyyy 'at' hh:mm a")
           : 'None';
         row.dateToUSigned = row.dateAcceptedTerms
-          ? `${formatDistanceToNow(parseISO(row.dateAcceptedTerms))} ago`
+          ? format(parseISO(row.dateAcceptedTerms), "MM-dd-yyyy 'at' hh:mm a")
           : 'None';
         row.orgs = row.roles
           ? row.roles
@@ -117,6 +117,7 @@ export const Users: React.FC = () => {
           : 'None';
         row.fullName = `${row.firstName} ${row.lastName}`;
       });
+      console.log(rows);
       if (user?.userType === 'globalAdmin') {
         setUsers(rows);
       } else if (user?.userType === 'regionalAdmin' && user?.regionId) {
@@ -199,7 +200,7 @@ export const Users: React.FC = () => {
       field: 'lastLoggedInString',
       headerName: 'Last Logged In',
       minWidth: 100,
-      flex: 0.75
+      flex: 1
     },
     {
       field: 'edit',
