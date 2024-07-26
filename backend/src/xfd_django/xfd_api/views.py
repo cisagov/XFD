4c530a6b77b5d79214c4284c3ba185c0a5c2e4d2
aloftus23 <andylofti@gmail.com>
2024-07-26 11:40:10 -0400

    Add authentication for organizations endpoint

--- backend/src/xfd_django/xfd_api/views.py
+++ backend/src/xfd_django/xfd_api/views.py
@@ -1,8 +1,6 @@
 
 
 from django.shortcuts import render
-
-# Third party imports
 from fastapi import (
     APIRouter,
     Depends,
@@ -13,7 +11,8 @@ from fastapi import (
     UploadFile,
     status,
 )
-from .models import ApiKey
+from .auth import get_current_active_user
+from .models import ApiKey, Organization, User
 
 api_router = APIRouter()
 
@@ -39,7 +38,6 @@ async def get_api_keys():
         list: A list of all API keys.
     """
     try:
-        print("API KEYS!!!!!!")
         api_keys = ApiKey.objects.all()
         # return api_keys
         return [
@@ -53,6 +51,42 @@ async def get_api_keys():
                 "user_id": api_key.userid.id if api_key.userid else None,
             }
             for api_key in api_keys
+        ]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@api_router.get("/organizations")
+async def get_organizations(current_user: User = Depends(get_current_active_user)):
+    """
+    Get all organizations.
+    Returns:
+        list: A list of all organizations.
+    """
+    try:
+        organizations = Organization.objects.all()
+        return [
+            {
+                "id": organization.id,
+                "name": organization.name,
+                "acronym": organization.acronym,
+                "root_domains": organization.rootdomains,
+                "ip_blocks": organization.ipblocks,
+                "is_passive": organization.ispassive,
+                "country": organization.country,
+                "state": organization.state,
+                "region_id": organization.regionid,
+                "state_fips": organization.statefips,
+                "state_name": organization.statename,
+                "county": organization.county,
+                "county_fips": organization.countyfips,
+                "type": organization.type,
+                "parent_id": organization.parentid.id if organization.parentid else None,
+                "created_by_id": organization.createdbyid.id if organization.createdbyid else None,
+                "created_at": organization.createdat,
+                "updated_at": organization.updatedat,
+            }
+            for organization in organizations
         ]
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
