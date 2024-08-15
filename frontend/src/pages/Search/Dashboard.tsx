ca5cadf67243befb31fd7ee03e0bcdc31bcd601c
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-08-15 10:15:07 -0400

    Removed commented out code from Dashboard.tsx

--- frontend/src/pages/Search/Dashboard.tsx
+++ frontend/src/pages/Search/Dashboard.tsx
@@ -165,8 +165,6 @@ export const DashboardUI: React.FC<ContextType & { location: any }> = (
       />
       <Box
         position="relative"
-        // flex="1 1 auto"
-        // height="100%"
         maxHeight="100%"
         width="100%"
         display="flex"
