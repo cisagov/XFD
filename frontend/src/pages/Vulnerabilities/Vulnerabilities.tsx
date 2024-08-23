97eefbc3499509add9bbd246c5bc7145cbd96512
Chrtorres <chris.rtorres@outlook.com>
2024-08-23 16:31:34 -0400

    Removed commented unused codee

--- frontend/src/pages/Vulnerabilities/Vulnerabilities.tsx
+++ frontend/src/pages/Vulnerabilities/Vulnerabilities.tsx
@@ -198,29 +198,6 @@ export const Vulnerabilities: React.FC<{ groupBy?: string }> = ({
   const history = useHistory();
   const location = useLocation();
   const state = location.state as LocationState;
-  // const paramsArray = [
-  //   [
-  //     {
-  //       field: 'title',
-  //       value: state.title,
-  //       operator: 'contains'
-  //     }
-  //   ],
-  //   [
-  //     {
-  //       field: 'domain',
-  //       value: state.domain,
-  //       operator: 'contains'
-  //     }
-  //   ],
-  //   [
-  //     {
-  //       field: 'severity',
-  //       value: state.severity,
-  //       operator: 'contains'
-  //     }
-  //   ]
-  // ];
   const [initialFilters, setInitialFilters] = useState<GridFilterItem[]>(
     state?.title
       ? [
