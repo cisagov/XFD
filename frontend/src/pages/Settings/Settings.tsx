8ed23c083600a9fe8bbfbd95271b244e1356b27c
Janson Bunce <janson.bunce@associates.cisa.dhs.gov>
2024-09-09 10:08:11 -0700

    initial commit - only shows the earliest role in /settings

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import classes from './Settings.module.css';
 import { useAuthContext } from 'context';
 import { Button } from '@trussworks/react-uswds';
 
 const Settings: React.FC = () => {
   const { logout, user } = useAuthContext();
+  console.log(
+    user?.roles.sort((a, b) => {
+      const first = new Date(a.createdAt);
+      const second = new Date(b.createdAt);
+      if (first > second) {
+        return 1;
+      }
+      return -1;
+    })
+  );
+
+  const earliestRole = useMemo(() => {
+    if (user) {
+      return user?.roles.sort((a, b) => {
+        const first = new Date(a.createdAt);
+        const second = new Date(b.createdAt);
+        if (first > second) {
+          return 1;
+        }
+        return -1;
+      })[0];
+    }
+    return null;
+  }, [user]);
 
   return (
     <div className={classes.root}>
@@ -13,11 +37,12 @@ const Settings: React.FC = () => {
       <h2>Email: {user && user.email}</h2>
       <h2>
         Member of:{' '}
-        {user &&
+        {/* {user &&
           (user.roles || [])
             .filter((role) => role.approved)
             .map((role) => role.organization.name)
-            .join(', ')}
+            .join(', ')} */}
+        {earliestRole ? earliestRole.organization.name : ''}
       </h2>
       <h2>Region: {user && user.regionId ? user.regionId : 'None'} </h2>
       <Button type="button" onClick={logout}>
