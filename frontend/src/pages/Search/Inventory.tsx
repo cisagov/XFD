0cd9799df0b2ed61ef38ec2a5a4b37f06072a05b
Chrtorres <chris.rtorres@outlook.com>
2024-09-09 16:13:42 -0400

    Code cleanup; remove unused useRef

--- frontend/src/pages/Search/Inventory.tsx
+++ frontend/src/pages/Search/Inventory.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { classes, Root } from './Styling/dashboardStyle';
 import { Subnav } from 'components';
 import { ResultCard } from './ResultCard';
