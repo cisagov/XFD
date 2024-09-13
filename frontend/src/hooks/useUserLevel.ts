c4eda34862f49c6cf9b828e30a5eabad06444bc0
aloftus23 <andylofti@gmail.com>
2024-09-13 08:48:51 -0400

    Add useUserLevel

---
+++ frontend/src/hooks/useUserLevel.ts
@@ -0,0 +1,49 @@
+import { AuthContextType, useAuthContext } from 'context';
+
+export const GLOBAL_ADMIN = 3;
+export const REGIONAL_ADMIN = 2;
+export const STANDARD_USER = 1;
+
+type UserType =
+  | 'standard'
+  | 'globalAdmin'
+  | 'regionalAdmin'
+  | 'globalView'
+  | 'readySetCyber'
+  | undefined
+  | null;
+
+type UserLevel = {
+  userLevel: number;
+  userType: UserType;
+  user: AuthContextType['user'];
+  formattedUserType: string;
+};
+
+export const useUserLevel: () => UserLevel = () => {
+  const { user } = useAuthContext();
+  let userLevel = 0;
+  let formattedUserType = '';
+  const userType: UserType = user?.userType;
+  if (user && user.isRegistered) {
+    if (user.userType === 'standard') {
+      userLevel = STANDARD_USER;
+      formattedUserType = 'Standard User';
+    } else if (user.userType === 'globalAdmin') {
+      userLevel = GLOBAL_ADMIN;
+      formattedUserType = 'Global Admin';
+    } else if (user.userType === 'regionalAdmin') {
+      userLevel = REGIONAL_ADMIN;
+      formattedUserType = 'Regional Admin';
+    } else if (user.userType === 'globalView') {
+      userLevel = REGIONAL_ADMIN;
+      formattedUserType = 'Global View';
+    }
+  }
+  return {
+    userLevel,
+    userType,
+    user,
+    formattedUserType
+  };
+};
