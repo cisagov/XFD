a24a09f45ae20f3c2dc07a99d2c15659aa4fded3
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-09-16 17:14:40 -0400

    Editd default placeholder text in Search bar
    - Removed mention of Port, CVE, or Severity Level as returned results do not consider these fields currently.
    - It is confusing to the user to have these fields mentioned in the placeholder text when they are not considered in the search results.

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -87,7 +87,7 @@ interface Props
   initialValue: string;
 }
 
-const defaultPlaceholder = 'Search a domain, vuln, port, service, IP';
+const defaultPlaceholder = 'Search for Domain or IP';
 
 type Timer = ReturnType<typeof setTimeout>;
 
