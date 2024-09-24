646c54201b112f9277b649811c26f61d7b18b46c
aloftus23 <andylofti@gmail.com>
2024-09-24 12:44:37 -0400

    Update bastion.ts

--- backend/src/tasks/bastion.ts
+++ backend/src/tasks/bastion.ts
@@ -1,5 +1,5 @@
 import { Handler } from 'aws-lambda';
-import { connectToDatabase, User } from '../models';
+import { connectToDatabase, User, connectToDatalake } from '../models';
 import ESClient from '../tasks/es-client';
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ region: 'us-gov-east-1' });
@@ -10,12 +10,22 @@ export const handler: Handler = async (event) => {
     const res = await connection.query(event.query);
     console.log(res);
     const params = {
-      Bucket: 'cisa-crossfeed-prod-exports', // replace with your bucket name
-      Key: 'bastion_output.json', // the file name you want to save as
+      Bucket: 'cisa-crossfeed-prod-exports',
+      Key: 'bastion_output.json',
       Body: JSON.stringify(res),
       ContentType: 'application/json'
     };
-
+    await s3.putObject(params).promise();
+  } else if (event.mode === 'mdl') {
+    const connection = await connectToDatalake(true);
+    const res = await connection.query(event.query);
+    console.log(res);
+    const params = {
+      Bucket: 'cisa-crossfeed-prod-exports',
+      Key: 'bastion_output.json',
+      Body: JSON.stringify(res),
+      ContentType: 'application/json'
+    };
     await s3.putObject(params).promise();
   } else if (event.mode === 'es') {
     if (event.query === 'delete') {
