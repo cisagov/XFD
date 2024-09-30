a6fc01ea331a7f98e4a91f5301ed7f086acf89ac
Amelia Vance <amelia.vance@associates.cisa.dhs.gov>
2024-09-30 16:22:14 -0400

    Replace link to roboto font in index.html with npm install and import in index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 //Refactoring needed for react 18:
 const rootElement = document.getElementById('root');
