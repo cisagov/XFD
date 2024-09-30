417fab83fe4386d5df104046039ac29a5c130fec
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2024-09-30 16:57:51 -0400

    Edited ToDo in RSCLogin.tsx

--- frontend/src/components/ReadySetCyber/RSCLogin.tsx
+++ frontend/src/components/ReadySetCyber/RSCLogin.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 
 export const RSCLogin: React.FC = () => {
-  // This is a placeholder for the Ready Set Cyber login page.
-  // The original legacy code utilized a registration and login system prior to the SSO Okta solution we now use.
-  // That system triggered vulnerabilities in the 08/24 ST&E Report.
-  // It was removed and replaced with this placeholder until an Okta/Login.gov integration is implemented for Ready Set Cyber in a future sprint.
-  // RSCRegisterForm.tsx and RSCAuthLoginCreate.tsx were also deleted.
-  // [CRASM-736](https://maestro.dhs.gov/jira/browse/CRASM-736)
+  // ToDo: [CRASM-736](https://maestro.dhs.gov/jira/browse/CRASM-736) Okta/Login.gov solution for Ready Set Cyber Dashboard
   return (
     <Box>
       <Typography variant="h1">Ready Set Cyber</Typography>
