28cf4cb5b56001c61fe780fa1ee11cc67ad01113
Thomas Johnson <123108455+hawkishpolicy@users.noreply.github.com>
2024-10-24 12:40:33 -0400

    Edite Cache-Control headrs in app.ts (#701)
    - Set to no-cache, no-store, must-revalidate

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -143,7 +143,7 @@ app.use(
 
 //Middleware to set Cache-Control headers
 app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'private, must-revalidate, max-age=3600');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   next();
 });
 
