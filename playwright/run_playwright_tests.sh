b8a35720bd80c5d952b03bdbe816acc87a26e8af
JCantu248 <jcantu248@gmail.com>
2025-10-01 10:39:35 -0500

    Changes to CICD files for new user roles

--- playwright/run_playwright_tests.sh
+++ playwright/run_playwright_tests.sh
@@ -19,14 +19,22 @@ OVERRIDES=$(jq -n \
   --arg bucket "$AUTOMATED_TEST_REPORTS_BUCKET_NAME" \
   --arg region "$AWS_REGION" \
   --arg url "$PW_XFD_URL" \
-  --arg username "$PW_XFD_USERNAME" \
-  --arg password "$PW_XFD_PASSWORD" \
-  --arg otpsecret "$PW_XFD_2FA_SECRET" \
-  --arg login "$PW_XFD_LOGIN" \
+  --arg global_admin_username "$PW_XFD_GLOBAL_ADMIN_USERNAME" \
+  --arg global_admin_password "$PW_XFD_GLOBAL_ADMIN_PASSWORD" \
+  --arg global_admin_2fa "$PW_XFD_GLOBAL_ADMIN_2FA_SECRET" \
+  --arg regional_admin_username "$PW_XFD_REGIONAL_ADMIN_USERNAME" \
+  --arg regional_admin_password "$PW_XFD_REGIONAL_ADMIN_PASSWORD" \
+  --arg regional_admin_2fa "$PW_XFD_REGIONAL_ADMIN_2FA_SECRET" \
+  --arg global_view_username "$PW_XFD_GLOBAL_VIEW_USERNAME" \
+  --arg global_view_password "$PW_XFD_GLOBAL_VIEW_PASSWORD" \
+  --arg global_view_2fa "$PW_XFD_GLOBAL_VIEW_2FA_SECRET" \
+  --arg standard_username "$PW_XFD_STANDARD_USER_USERNAME" \
+  --arg standard_password "$PW_XFD_STANDARD_USER_PASSWORD" \
+  --arg standard_2fa "$PW_XFD_STANDARD_USER_2FA_SECRET" \
   --arg git_branch "$GIT_BRANCH" \
   --arg environment "$ENVIRONMENT" \
   --arg headless "$PW_HEADLESS" \
-  --arg ci "$PW_CI" \
+  --arg ci "$CI" \
   --arg s3HtmlPath "$S3_HTML_PATH" \
   --arg s3JsonPath "$S3_JSON_PATH" \
   '{
@@ -38,14 +46,24 @@ OVERRIDES=$(jq -n \
           { "name": "AUTOMATED_TEST_REPORTS_BUCKET_NAME", "value": $bucket },
           { "name": "AWS_REGION", "value": $region },
           { "name": "PW_XFD_URL", "value": $url },
-          { "name": "PW_XFD_USERNAME", "value": $username },
+          { "name": "PW_XFD_GLOBAL_ADMIN_USERNAME", "value": $global_admin_username },
+          { "name": "PW_XFD_GLOBAL_ADMIN_PASSWORD", "value": $global_admin_password },
+          { "name": "PW_XFD_GLOBAL_ADMIN_2FA_SECRET", "value": $global_admin_2fa },
+          { "name": "PW_XFD_REGIONAL_ADMIN_USERNAME", "value": $regional_admin_username },
+          { "name": "PW_XFD_REGIONAL_ADMIN_PASSWORD", "value": $regional_admin_password },
+          { "name": "PW_XFD_REGIONAL_ADMIN_2FA_SECRET", "value": $regional_admin_2fa },
+          { "name": "PW_XFD_GLOBAL_VIEW_USERNAME", "value": $global_view_username },
+          { "name": "PW_XFD_GLOBAL_VIEW_PASSWORD", "value": $global_view_password },
+          { "name": "PW_XFD_GLOBAL_VIEW_2FA_SECRET", "value": $global_view_2fa },
+          { "name": "PW_XFD_STANDARD_USER_USERNAME", "value": $standard_username },
+          { "name": "PW_XFD_STANDARD_USER_PASSWORD", "value": $standard_password },
+          { "name": "PW_XFD_STANDARD_USER_2FA_SECRET", "value": $standard_2fa },
           { "name": "PW_XFD_PASSWORD", "value": $password },
           { "name": "PW_XFD_2FA_SECRET", "value": $otpsecret },
-          { "name": "PW_XFD_LOGIN", "value": $login },
           { "name": "GIT_BRANCH", "value": $git_branch },
           { "name": "ENVIRONMENT", "value": $environment },
           { "name": "PW_HEADLESS", "value": $headless },
-          { "name": "PW_CI", "value": $ci },
+          { "name": "CI", "value": $ci },
           { "name": "S3_HTML_PATH", "value": $s3HtmlPath },
           { "name": "S3_JSON_PATH", "value": $s3JsonPath }
         ]
