b2322cd376c42d2b0e9fd74746b4c3750ea49335
Jesse Salinas <jesse.salinas@nttdatafed.com>
2025-10-01 13:39:30 -0700

    refs CRASM-3004: Fix filter clearing logic to preserve region and organization filters
    - Modified useClearFiltersOnMount hook to only clear non-organization and non-region filters
    - This allows region and organization filters to persist when returning to VS Dashboard
    - Fixed index.ts export to reference correct VulnerabilityScan component

--- frontend/src/hooks/useClearFiltersOnMount.ts
+++ frontend/src/hooks/useClearFiltersOnMount.ts
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
+import { ORGANIZATION_FILTER_KEY } from 'components/FilterDrawer/VSDashRegionAndOrgFilters';
 
 export function useClearFiltersOnMount(filters: any[], removeFilter: Function) {
   useEffect(() => {
+    // Only clear non-organization filters when mounting VulnerabilityScan
     filters.forEach((filter) => {
-      removeFilter(filter.field, filter.values[0], filter.type);
+      if (filter.field !== ORGANIZATION_FILTER_KEY && filter.field !== 'organization.region_id') {
+        removeFilter(filter.field, filter.values[0], filter.type);
+      }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
