cfaf610c8742e0e40b82b85b0fe96a5d3e2dfdf2
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2025-10-27 15:27:26 -0400

    Remove unused location imports from TermsGate

--- frontend/src/components/Gates/TermsGate.tsx
+++ frontend/src/components/Gates/TermsGate.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import { useAuthContext } from 'context/AuthContext';
 import { TermsOfUse } from 'components/Dialog/TermsOfUse/TermsOfUse';
 
@@ -9,7 +8,6 @@ interface TermsGateProps {
 
 const TermsGate: React.FC<TermsGateProps> = ({ children }) => {
   const { user, userMustSign } = useAuthContext();
-  const location = useLocation();
 
   if (user?.isRegistered && !user?.invite_pending && userMustSign) {
     return <TermsOfUse />;
