eb50ce1b0946ea908544b7bcf08eea4aad8e5ff7
Amelia Vance <amelia.vance@associates.cisa.dhs.gov>
2025-11-20 13:48:28 -0500

    Add folder to ignore in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 
 export default [
   {
-    ignores: ['tests/**', 'assets']
+    ignores: ['tests/**', 'assets', 'dist/**']
   },
 
   // TypeScript files (*.ts, *.tsx)
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,7 +7,7 @@ import unusedImportsPlugin from 'eslint-plugin-unused-imports';
 
 export default [
   {
-    ignores: ['tests/**', 'assets']
+    ignores: ['tests/**', 'assets', 'dist/**']
   },
 
   // TypeScript files (*.ts, *.tsx)
