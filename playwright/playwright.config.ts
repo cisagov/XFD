a0a775618fab0489e6110c06d705ee07379ae362
JCantu248 <jcantu248@gmail.com>
2025-12-11 23:37:02 -0600

    Add playwright configuration tweaks for performance and test reliability.

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -30,9 +30,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 3,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: reporters,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
