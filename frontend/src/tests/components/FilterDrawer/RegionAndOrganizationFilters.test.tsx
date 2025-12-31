5f62bda93aa35ba9ac9f4d47a44db78e3a9ad47b
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2025-12-31 10:40:45 -0500

    Add additional unit tests and utils:
    - removed apiPatch from authCtx mock as we no longer use it in the app.
    - created searchOrg folder in test-utils for holding mock /search/organization data.
    - added additional test users to user.ts for testing different roles.
    - added battery of tests to RegionAndOrganizationFilters.test.tsx to cover region/organization filter functionality in FilterDrawer component.
      - rendering of checkbox controlled lists for regions if user is an admin.
      - rendering of disabled region autocomplete with user region id if user is a standard user.
      - rendering of organization autocomplete containing orgs from mock data if user is a global admin.
    - added await to scanningSummary.test.tsx to ensure async operations complete before assertions.

--- frontend/src/tests/components/FilterDrawer/RegionAndOrganizationFilters.test.tsx
+++ frontend/src/tests/components/FilterDrawer/RegionAndOrganizationFilters.test.tsx
@@ -1,37 +1,191 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, expect, it, vi, beforeEach } from 'vitest';
+import { render } from '@/test-utils/test-utils';
+import { screen } from '@testing-library/react';
 import { RegionAndOrganizationFilters } from '../../../components/FilterDrawer/RegionAndOrganizationFilters';
+import { useStaticsContext } from '@/context/StaticsContext';
+import { useAuthContext } from '@/context/AuthContext';
+import { authCtx } from '@/test-utils/authCtx';
+import {
+  regionalAdminUser,
+  globalViewUser,
+  globalAdminUser
+} from '@/test-utils';
+import { mockOrganizations } from '@/test-utils/searchOrg';
+import userEvent from '@testing-library/user-event';
 
-const mockOnRegionChange = vi.fn();
-const mockOnOrganizationChange = vi.fn();
+//Mock hooks
+vi.mock('context/AuthContext');
+vi.mock('context/StaticsContext');
 
-//   beforeEach(() => {
-//     render(
-//       <RegionAndOrganizationFilters
-//         regions={mockRegions}
-//         organizations={mockOrganizations}
-//         selectedRegions={[]}
-//         selectedOrganizations={[]}
-//         onRegionChange={mockOnRegionChange}
-//         onOrganizationChange={mockOnOrganizationChange}
-//       />
-//     );
-//   });
 describe('RegionAndOrganizationFilters Component', () => {
-  const mockRegions = [
-    { id: '1', name: 'North America' },
-    { id: '2', name: 'Europe' }
-  ];
+  const defaultProps = {
+    addFilter: vi.fn(),
+    removeFilter: vi.fn(),
+    filters: [],
+    setSearchTerm: vi.fn(),
+    searchTerm: '',
+    autocompletedResults: [],
+    autocompletedSuggestions: [],
+    results: [],
+    initialFilters: [],
+    expanded: false as string | false | undefined
+  };
 
-  const mockOrganizations = [
-    { id: '1', name: 'Org A' },
-    { id: '2', name: 'Org B' }
-  ];
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-  it('renders region filters correctly', () => {
-    mockRegions.forEach((region) => {
-      expect(screen.getByLabelText(region.name)).toBeInTheDocument();
+  describe('Region Filters Global Admin User', () => {
+    it('renders all region filters correctly', async () => {
+      vi.mocked(useStaticsContext).mockReturnValue({
+        regions: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+        setRegions: vi.fn()
+      });
+      const globalAdminAuthCtx = { ...authCtx, user: globalAdminUser };
+      vi.mocked(useAuthContext).mockReturnValue(globalAdminAuthCtx);
+      render(<RegionAndOrganizationFilters {...defaultProps} />);
+
+      const regionAccordion = screen.getByText('Regions');
+      expect(regionAccordion).toBeInTheDocument();
+
+      // Check if region filter options are rendered
+      expect(screen.getByText('All Regions')).toBeInTheDocument();
+      expect(screen.getByText('Region 1')).toBeInTheDocument();
+      expect(screen.getByText('Region 2')).toBeInTheDocument();
+      expect(screen.getByText('Region 3')).toBeInTheDocument();
+      expect(screen.getByText('Region 4')).toBeInTheDocument();
+      expect(screen.getByText('Region 5')).toBeInTheDocument();
+      expect(screen.getByText('Region 6')).toBeInTheDocument();
+      expect(screen.getByText('Region 7')).toBeInTheDocument();
+      expect(screen.getByText('Region 8')).toBeInTheDocument();
+      expect(screen.getByText('Region 9')).toBeInTheDocument();
+      expect(screen.getByText('Region 10')).toBeInTheDocument();
+    });
+  });
+
+  describe('Region Filters Global View User', () => {
+    it('renders all region filters correctly', async () => {
+      vi.mocked(useStaticsContext).mockReturnValue({
+        regions: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+        setRegions: vi.fn()
+      });
+      const globalViewAuthCtx = {
+        ...authCtx,
+        user: globalViewUser
+      };
+      vi.mocked(useAuthContext).mockReturnValue(globalViewAuthCtx);
+      render(<RegionAndOrganizationFilters {...defaultProps} />);
+
+      const regionAccordion = screen.getByText('Regions');
+      expect(regionAccordion).toBeInTheDocument();
+
+      // Check if region filter options are rendered
+      expect(screen.getByText('All Regions')).toBeInTheDocument();
+      expect(screen.getByText('Region 1')).toBeInTheDocument();
+      expect(screen.getByText('Region 2')).toBeInTheDocument();
+      expect(screen.getByText('Region 3')).toBeInTheDocument();
+      expect(screen.getByText('Region 4')).toBeInTheDocument();
+      expect(screen.getByText('Region 5')).toBeInTheDocument();
+      expect(screen.getByText('Region 6')).toBeInTheDocument();
+      expect(screen.getByText('Region 7')).toBeInTheDocument();
+      expect(screen.getByText('Region 8')).toBeInTheDocument();
+      expect(screen.getByText('Region 9')).toBeInTheDocument();
+      expect(screen.getByText('Region 10')).toBeInTheDocument();
+    });
+  });
+
+  describe('Region Filters Regional Admin User', () => {
+    it('renders all region filters correctly', async () => {
+      vi.mocked(useStaticsContext).mockReturnValue({
+        regions: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+        setRegions: vi.fn()
+      });
+      const regionalAdminAuthCtx = {
+        ...authCtx,
+        user: regionalAdminUser
+      };
+      vi.mocked(useAuthContext).mockReturnValue(regionalAdminAuthCtx);
+      render(<RegionAndOrganizationFilters {...defaultProps} />);
+
+      const regionAccordion = screen.getByText('Regions');
+      expect(regionAccordion).toBeInTheDocument();
+
+      // Check if region filter options are rendered
+      expect(screen.getByText('All Regions')).toBeInTheDocument();
+      expect(screen.getByText('Region 1')).toBeInTheDocument();
+      expect(screen.getByText('Region 2')).toBeInTheDocument();
+      expect(screen.getByText('Region 3')).toBeInTheDocument();
+      expect(screen.getByText('Region 4')).toBeInTheDocument();
+      expect(screen.getByText('Region 5')).toBeInTheDocument();
+      expect(screen.getByText('Region 6')).toBeInTheDocument();
+      expect(screen.getByText('Region 7')).toBeInTheDocument();
+      expect(screen.getByText('Region 8')).toBeInTheDocument();
+      expect(screen.getByText('Region 9')).toBeInTheDocument();
+      expect(screen.getByText('Region 10')).toBeInTheDocument();
+    });
+  });
+
+  describe('Region Filter Standard User', () => {
+    it('renders authroized region filters only', async () => {
+      vi.mocked(useStaticsContext).mockReturnValue({
+        regions: ['3'],
+        setRegions: vi.fn()
+      });
+      // Test User in authCtx has access to region "3" only
+      vi.mocked(useAuthContext).mockReturnValue(authCtx);
+      render(<RegionAndOrganizationFilters {...defaultProps} />);
+      const regionAccordion = screen.getByText('Regions');
+      expect(regionAccordion).toBeInTheDocument();
+
+      const disabledAutoCompleteWithUserRegion =
+        screen.getByLabelText('Region 3');
+      expect(disabledAutoCompleteWithUserRegion).toBeDisabled();
+      expect(disabledAutoCompleteWithUserRegion).toBeInTheDocument();
+    });
+  });
+
+  describe('Organization Filters Global Admin User', () => {
+    it('renders organization filters correctly', async () => {
+      const globalAdminAuthCtx = {
+        ...authCtx,
+        user: globalAdminUser,
+        apiPost: vi.fn().mockResolvedValue({
+          body: {
+            hits: {
+              hits: mockOrganizations.map((org) => ({ _source: org }))
+            }
+          }
+        })
+      };
+      vi.mocked(useAuthContext).mockReturnValue(globalAdminAuthCtx);
+      render(<RegionAndOrganizationFilters {...defaultProps} />);
+
+      const organizationAccordion = screen.getByText('Organizations');
+      expect(organizationAccordion).toBeInTheDocument();
+
+      const user = userEvent.setup();
+      await user.click(organizationAccordion);
+
+      const orgAutoComplete = screen.getByLabelText('Search Organizations');
+      expect(orgAutoComplete).toBeInTheDocument();
+
+      await user.click(orgAutoComplete);
+      screen.debug();
+      // Check if organization filter options are rendered
+
+      expect(
+        await screen.findByText('Organization 1 (ORG1)')
+      ).toBeInTheDocument();
+      expect(
+        await screen.findByText('Organization 2 (ORG2)')
+      ).toBeInTheDocument();
+      expect(
+        await screen.findByText('Organization 3 (ORG3)')
+      ).toBeInTheDocument();
+      expect(
+        await screen.findByText('Organization 4 (ORG4)')
+      ).toBeInTheDocument();
     });
   });
 });
