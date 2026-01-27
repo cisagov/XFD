9a0d5de73c95505aeead09709e3e6312cc02b4d6
Jesse Salinas <jesse.salinas@nttdatafed.com>
2026-01-27 11:16:53 -0700

    refs CRASM-3612: Code cleanup

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -5,7 +5,6 @@ export * from './AuthRoute';
 export * from './Routes/RouteGuard';
 export * from './Routes/Routes';
 export * from '../pages/Domains/DomainDetails';
-
 export * from './FilterDrawer/FacetFilter';
 export * from './FindingsLibrary/Subnav';
 export * from './ModalToggleButton';
