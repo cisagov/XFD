6b7b98bea795e3964bbb9e0317898b7dfc5bc029
nickviola <nick.viola@associates.cisa.dhs.gov>
2026-01-30 10:21:52 -0600

    Remove commented code from AuthRoute.tsx and RouteGaurd.tsx

--- frontend/src/components/AuthRoute/AuthRoute.tsx
+++ frontend/src/components/AuthRoute/AuthRoute.tsx
@@ -13,13 +13,7 @@ export const AuthRedirectRoute: React.FC<AuthRedirectRouteProps> = ({
   component,
   ...rest
 }) => {
-  // const token = localStorage.getItem('token');
-
   const { user } = useAuthContext();
-
-  // if (token && !user) {
-  //   return null;
-  // }
 
   const RedirectComponent = () => (
     <Redirect to={{ pathname: redirectUrl, state: { authRequired: true } }} />
