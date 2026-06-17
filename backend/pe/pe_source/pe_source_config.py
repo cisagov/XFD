ba5d22b87401bd3a79ceaa6549e489e14a7889ef
arng4108 <arng4108@gmail.com>
2026-06-17 11:19:23 -0400

    Additional linting for pe_source_config.py

--- backend/pe/pe_source/pe_source_config.py
+++ backend/pe/pe_source/pe_source_config.py
@@ -1,5 +1,6 @@
 """File for managing parameters and authentication."""
 
+# Standard Python Libraries
 from configparser import ConfigParser
 import os
 
@@ -7,6 +8,7 @@ import os
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 ini_path = os.path.join(curr_dir, "..", "pe_reports", "parameters.ini")
 INI_FILE = os.path.normpath(ini_path)
+
 
 def get_params(section):
     """Retrieve parameters from the specified section of the central ini file."""
@@ -19,10 +21,8 @@ def get_params(section):
             params = dict(parser.items(section))
         else:
             raise Exception(
-                "Section \"{}\" not found in the \"{}\" file".format(section, INI_FILE)
+                'Section "{}" not found in the "{}" file'.format(section, INI_FILE)
             )
     else:
-        raise Exception(
-            "File not found at this path: \"{}\"".format(INI_FILE)
-        )
+        raise Exception('File not found at this path: "{}"'.format(INI_FILE))
     return params
