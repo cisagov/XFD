5b981fb81c85d70bf5a2e7067a9c89b3307442bb
Amelia Vance <amelia.vance@associates.cisa.dhs.gov>
2026-06-26 09:35:41 -0400

    Add precise number regex to dhs region org check

--- frontend/src/components/Gates/VSDashboardGate.tsx
+++ frontend/src/components/Gates/VSDashboardGate.tsx
@@ -75,9 +75,10 @@ export const VSDashboardGate: React.FC<{
   if (error || isDataEmpty(vulnScanData)) {
     const noDataUserType =
       error === 'NO_DATA' ? 'standard' : userType || 'standard';
+    const endNumRegex = /\s\b([1-9]|10)$/;
     return (
       <PageSection>
-        {orgName.startsWith('DHS Region') && (
+        {orgName.startsWith('DHS Region') && endNumRegex.test(orgName) && (
           <CustomAlert
             headerMsg="Welcome to CyHy Dashboard"
             bodyMsg="Based on your profile you will need to select a different organization in order to populate the Dashboard."
