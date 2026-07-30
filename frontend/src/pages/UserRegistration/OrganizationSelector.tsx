b44e6006e1465abd91b2721d0b13f3baa32e2365
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2026-07-30 11:40:04 -0400

    Update the OrganizationSelector component:
    - Replaced the DataGrid component with a CustomDataGrid.
    - Updated import statement formatting and order.

--- frontend/src/pages/UserRegistration/OrganizationSelector.tsx
+++ frontend/src/pages/UserRegistration/OrganizationSelector.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { DataGrid, GridRowSelectionModel, GridToolbar } from '@mui/x-data-grid';
+import { GridRowSelectionModel, GridToolbar } from '@mui/x-data-grid';
+
+// Material-UI Components
 import Alert from '@mui/material/Alert';
 import Autocomplete from '@mui/material/Autocomplete';
 import Grid from '@mui/material/Grid';
@@ -9,13 +11,22 @@ import RadioGroup from '@mui/material/RadioGroup';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+
+// Types
 import { Organization as OrganizationType } from 'types';
-import { ENDPOINTS } from '@/constants/endpoints';
-import { useAuthContext } from 'context';
-import { organizationCols as orgCols } from './UserRegistrationColumns';
 import { User } from 'types';
-import { REGION_STATE_MAP, STATE_OPTIONS } from '@/constants/constants';
+
+// Components
+import CustomDataGrid from '@/components/DataGrid/CustomDataGrid';
 import { ElevationControl } from '../Users/ElevationControl';
+import { organizationCols as orgCols } from './UserRegistrationColumns';
+
+// Constants
+import { ENDPOINTS } from '@/constants/endpoints';
+import { REGION_STATE_MAP, STATE_OPTIONS } from '@/constants/constants';
+
+// Context
+import { useAuthContext } from 'context';
 export interface OrganizationSelectorProps {
   regionId: string | null | undefined;
   onSelectionChange: (organization: OrganizationType | null) => void;
@@ -216,7 +227,7 @@ export const OrganizationSelector: React.FC<OrganizationSelectorProps> = ({
         </Grid>
       </Grid>
       <Paper sx={{ height: 400, margin: 'auto' }}>
-        <DataGrid
+        <CustomDataGrid
           checkboxSelection
           onRowSelectionModelChange={onRowSelectionModelChange}
           rowSelectionModel={localSelectedOrg}
