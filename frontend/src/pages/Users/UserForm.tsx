a8ec75441374940ebd947aac429a636fe6a8ce47
Thomas <thomas.j.johnson@associates.cisa.dhs.gov>
2026-09-15 16:26:16 -0400

    Remove e.detail & e.payload.detail from UserForm:
    - new error message construction sets e.message with detail from the payload, response status text, or a default message.
    - this makes calling e.message and e.detail redundant, as e.message now contains all relevant information.

--- frontend/src/pages/Users/UserForm.tsx
+++ frontend/src/pages/Users/UserForm.tsx
@@ -293,10 +293,9 @@ export const UserForm: React.FC<UserFormProps> = ({
       setInfoDialogContent('This user has been successfully updated.');
       setInfoDialogOpen(true);
     } catch (error: any) {
-      const detail = error?.detail || error?.payload?.detail;
       setApiErrorStates({
         ...apiErrorStates,
-        getUpdateUserError: error.message + (detail ? `. ${detail}` : '')
+        getUpdateUserError: error.message
       });
       setInfoDialogContent(
         'This user has not been updated. Check the console log for more details.'
